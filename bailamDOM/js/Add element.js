e87adeecb44eec1bfb6740c5812d1c84c0123abe
nguyendacdung1 <nguyendungmk9@gmail.com>
2021-11-22 12:13:28 +0700

    baidom

---
+++ bailamDOM/js/Add element.js
@@ -0,0 +1,5 @@
+var newEl=document.createElement('li');
+var newText=document.createTextNode('quinoa');
+newEl.appendChild(newText)
+var posision=document.getElementsByTagName('ul')[0];
+posision.appendChild(newEl);
